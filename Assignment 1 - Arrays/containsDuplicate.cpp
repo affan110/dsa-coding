4b2fcbefcd58a7abb5c17e4a9e4e83ea55f9dedc
affan110 <97936529+affan110@users.noreply.github.com>
2023-06-24 21:29:25 +0530

    Update containsDuplicate.cpp

--- Assignment 1 - Arrays/containsDuplicate.cpp
+++ Assignment 1 - Arrays/containsDuplicate.cpp
@@ -1,12 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 bool containsDuplicate(vector<int>& nums) {
-        // Sort the nums...
-        sort(nums.begin(), nums.end());
-        // Traverse all the elements through the loop...
-        for(int idx = 0; idx < nums.size() - 1; idx++) {
-            // Check the duplicate element...
-            if(nums[idx] == nums[idx + 1])
-                return true;
-        }
-       
-        return false;
+    sort(nums.begin(), nums.end());
+
+    for (int idx = 0; idx < nums.size() - 1; idx++) {
+        if (nums[idx] == nums[idx + 1])
+            return true;
     }
+
+    return false;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 1};
+
+    bool result = containsDuplicate(nums);
+    cout << "Contains duplicate: " << (result ? "true" : "false") <<endl;
+
+    return 0;
+}
