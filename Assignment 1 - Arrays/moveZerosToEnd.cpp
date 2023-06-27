7d60d0dfa13a273811897f11460bb817f7b5a72e
affan110 <97936529+affan110@users.noreply.github.com>
2023-06-27 15:36:34 +0530

    Update moveZerosToEnd.cpp

--- Assignment 1 - Arrays/moveZerosToEnd.cpp
+++ Assignment 1 - Arrays/moveZerosToEnd.cpp
@@ -1,14 +1,39 @@
-class Solution {
-public:
-    void moveZeroesToEnd(vector<int>& nums) {
-        int n= nums.size();
-        int start=0, end=n-1,mid=0;
-        while(mid<=end){
-            if(nums[mid]!=0){
-                swap(nums[start],nums[mid]);                  
-                    start++;
-            }
-            mid++;
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void moveZeroesToEnd(vector<int>& nums) {
+    int n = nums.size();
+    int nonZeroEle = 0;
+    for (int i = 0; i < n; i++) {
+        if (nums[i] != 0) {
+            nums[nonZeroEle] = nums[i];
+            nonZeroEle++;
         }
     }
-};
+    for (int i = nonZeroEle; i < n; i++) {
+        nums[i] = 0;
+    }
+}
+
+void display(vector<int>& nums) {
+    for (int i=0;i<nums.size();i++) {
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+}
+
+int main() {
+    vector<int> nums = {0, 1, 0, 3, 12};
+
+    
+    display(nums);
+    //calling function
+    moveZeroesToEnd(nums);
+
+    cout << "Final Output: ";
+    //print
+    display(nums);
+
+    return 0;
+}
